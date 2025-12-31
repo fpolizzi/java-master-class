7cd2d440667fbf4f582f0f04d14ba28280c46754
Franco Polizzi <me@fpolizzi.com>
2025-12-31 20:52:32 +0100

    feat: add CarBookingDao

---
+++ src/main/java/com/polizzi/booking/CarBookingDao.java
@@ -0,0 +1,50 @@
+package com.polizzi.booking;
+
+import java.util.UUID;
+
+/**
+ * Created by fpolizzi on 31.12.25
+ */
+public class CarBookingDao {
+
+    private final static CarBooking[] carBookings;
+
+    static {
+        carBookings = new CarBooking[10];
+    }
+
+    public CarBooking[] getCarBookings() {
+        return carBookings;
+    }
+
+    public void book(CarBooking carBooking) {
+        int nextFreeIndex = -1;
+
+        for (int i = 0; i < carBookings.length; i++) {
+            if (carBookings[i] == null) {
+                nextFreeIndex = i;
+            }
+        }
+
+        if (nextFreeIndex > -1) {
+            carBookings[nextFreeIndex] = carBooking;
+            return;
+        }
+
+        // full array
+        // copy all bookings to a new array with bigger space
+        CarBooking[] biggerCarBookings = new CarBooking[carBookings.length + 10];
+
+        for (int i = 0; i < carBookings.length; i++) {
+            biggerCarBookings[i] = carBookings[i];
+        }
+
+        // add finally add new booking
+        biggerCarBookings[carBookings.length] = carBooking;
+
+    }
+
+    public void cancelCarBooking(UUID id) {
+
+    }
+}
