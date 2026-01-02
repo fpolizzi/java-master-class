6bb3c94e01b185664cf820675024bf2e578cc678
Franco Polizzi <me@fpolizzi.com>
2026-01-02 19:54:05 +0100

    feat: implement remaining functionalities into menu

--- src/main/java/com/polizzi/Main.java
+++ src/main/java/com/polizzi/Main.java
@@ -1,59 +1,56 @@
 package com.polizzi;
 // DONE 1. create a new branch called initial-implementation
-// DONE 2. create a package with your name. i.e com.franco and move this file inside the new package
-// TODO 3. implement https://amigoscode.com/learn/java-cli-build/lectures/3a83ecf3-e837-4ae5-85a8-f8ae3f60f7f5
+// DONE 2. create a package with your name. i.e., com.franco and move this file inside the new package
+// DONE 3. implement https://amigoscode.com/learn/java-cli-build/lectures/3a83ecf3-e837-4ae5-85a8-f8ae3f60f7f05
 
+import com.polizzi.booking.CarBooking;
+import com.polizzi.booking.CarBookingService;
 import com.polizzi.car.Car;
-import com.polizzi.car.CarService;
 import com.polizzi.user.User;
 import com.polizzi.user.UserService;
 
 import java.util.Scanner;
+import java.util.UUID;
 
 public class Main {
 
     public static void main(String[] args) {
 
         UserService userService = new UserService();
-        CarService carService = new CarService();
+        CarBookingService carBookingService = new CarBookingService();
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nJava Master Class - Car Booking System\n");
+        boolean keepLooping = true;
 
-        while (true) {
-            displayMainMenu();
-
-            int choice = getUserChoice(scanner);
-
-            switch (choice) {
-                case 1:
-                    // TODO: Implement book car functionality
-                    break;
-                case 2:
-                    // TODO: Implement view all user booked cars functionality
-                    break;
-                case 3:
-                    // TODO: Implement view all bookings functionality
-                    break;
-                case 4:
-                    displayAllAvailableCars(carService);
-                    break;
-                case 5:
-                    displayAllAvailableElectricCars(carService);
-                    System.out.println();
-                    break;
-                case 6:
-                    displayAllUsers(userService);
-                    System.out.println();
-                    break;
-                case 7:
-                    System.out.println("\nThank you for using Car Booking System!");
-                    scanner.close();
-                    return;
-                default:
-                    System.out.println("\nInvalid choice! Use an integer from 1 to 7.\n");
+        while (keepLooping) {
+            try {
+                displayMenu();
+                String choice = scanner.nextLine();
+                switch (Integer.parseInt(choice)) {
+                    case 1 -> bookCar(userService, carBookingService, scanner);
+                    case 2 -> displayAllUserBookedCars(userService, carBookingService, scanner);
+                    case 3 -> allBookings(carBookingService);
+                    case 4 -> displayAvailableCars(carBookingService, false);
+                    case 5 -> displayAvailableCars(carBookingService, true);
+                    case 6 -> displayAllUsers(userService);
+                    case 7 -> keepLooping = false;
+                    default -> System.out.println(choice + " not a valid option ❌");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
+        }
+    }
+
+    private static void allBookings(CarBookingService carBookingService) {
+        CarBooking[] bookings = carBookingService.getBookings();
+        if (bookings.length == 0) {
+            System.out.println("No bookings available 😕");
+            return;
+        }
+        for (CarBooking booking : bookings) {
+            System.out.println("booking = " + booking);
         }
     }
 
@@ -68,48 +65,80 @@ public class Main {
         }
     }
 
-    private static void displayAllAvailableCars(CarService carService) {
-        Car[] cars = carService.getAllCars();
-        if (cars.length == 0) {
-            System.out.println("❌ No cars available");
+    private static void displayAvailableCars(CarBookingService carBookingService, boolean isElectric) {
+        Car[] availableCars = isElectric ? carBookingService.getAvailableElectricCars() : carBookingService.getAvailableCars();
+        if (availableCars.length == 0) {
+            System.out.println("❌ No cars available for renting");
             return;
         }
-        for (Car car : cars) {
-            System.out.println(car);
+        for (Car availableCar : availableCars) {
+            System.out.println(availableCar);
         }
     }
 
-    private static void displayAllAvailableElectricCars(CarService carService) {
-        Car[] electricCars = carService.getAllElectricCars();
-        if (electricCars.length == 0) {
-            System.out.println("❌ No electric cars available");
+    private static void displayAllUserBookedCars(UserService userService,
+                                                 CarBookingService carBookingService,
+                                                 Scanner scanner) {
+        displayAllUsers(userService);
+
+        System.out.println("➡️ select user id");
+        String userId = scanner.nextLine();
+
+        User user = userService.getUserById(UUID.fromString(userId));
+        if (user == null) {
+            System.out.println("❌ No user found with id " + userId);
             return;
         }
-        for (Car car : electricCars) {
-            System.out.println(car);
+
+        Car[] userBookedCars = carBookingService.getUserBookedCars(user.getId());
+        if (userBookedCars.length == 0) {
+            System.out.printf("❌ user %s has no cars booked", user);
+            return;
+        }
+        for (Car userBookedCar : userBookedCars) {
+            System.out.println(userBookedCar);
         }
     }
 
-    public static void displayMainMenu() {
+    private static void bookCar(UserService userService, CarBookingService carBookingService, Scanner scanner) {
+        displayAvailableCars(carBookingService, false);
 
-        System.out.println(" 1 - Book Car");
-        System.out.println(" 2 - View All User Booked Cars");
-        System.out.println(" 3 - View All Bookings");
-        System.out.println(" 4 - View Available Cars");
-        System.out.println(" 5 - View Available Electric Cars");
-        System.out.println(" 6 - View all users");
-        System.out.println(" 7 - Exit");
+        System.out.println("➡️ select car reg number");
+        String regNumber = scanner.nextLine();
+
+        displayAllUsers(userService);
+
+        System.out.println("➡️ select user id");
+        String userId = scanner.nextLine();
+
+        try {
+            User user = userService.getUserById(UUID.fromString(userId));
+            if (user == null) {
+                System.out.println("❌ No user found with id " + userId);
+            } else {
+                UUID bookingId = carBookingService.bookCar(user, regNumber);
+                String confirmationMessage = """
+                        🎉 Successfully booked car with reg number %s for user %s
+                        Booking ref: %s
+                        """.formatted(regNumber, user, bookingId);
+                System.out.println(confirmationMessage);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    public static int getUserChoice(Scanner scanner) {
-
-        System.out.print("\nEnter your choice: ");
-        if (scanner.hasNextInt()) {
-            return scanner.nextInt();
-        }
-
-        scanner.next();
-
-        return 0;
+    private static void displayMenu() {
+        System.out.println("""
+                \n
+                1 - Book Car
+                2 - View All User Booked Cars
+                3 - View All Bookings
+                4 - View Available Cars
+                5 - View Available Electric Cars
+                6 - View all users
+                7 - Exit
+                """);
     }
 }
