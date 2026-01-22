98e58f1cd3ee9b9da556b2315145c258fb4b731e
Franco Polizzi <me@fpolizzi.com>
2026-01-22 22:46:40 +0100

    refactor: replace array by List and ArrayList for user package

--- src/main/java/com/fpolizzi/user/UserArrayDataAccessService.java
+++ src/main/java/com/fpolizzi/user/UserArrayDataAccessService.java
@@ -1,23 +1,24 @@
 package com.fpolizzi.user;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 /**
  * Created by fpolizzi on 06.01.26
  */
-public class UserArrayDataAccessService implements UserDao{
+public class UserArrayDataAccessService implements UserDao {
 
-    private static final User[] users;
+    private static final List<User> users;
 
     static {
-        users = new User[]{
-                new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "James", "Meyers"),
-                new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Jamila", "Dialo")
-        };
+        users = new ArrayList<>();
+        users.add(new User(UUID.fromString("8ca51d2b-aaaf-4bf2-834a-e02964e10fc3"), "James", "Meyers"));
+        users.add(new User(UUID.fromString("b10d126a-3608-4980-9f9c-aa179f5cebc3"), "Jamila", "Dialo"));
     }
 
     @Override
-    public User[] getUsers() {
+    public List<User> getUsers() {
         return users;
     }
 }
--- src/main/java/com/fpolizzi/user/UserDao.java
+++ src/main/java/com/fpolizzi/user/UserDao.java
@@ -1,9 +1,11 @@
 package com.fpolizzi.user;
 
+import java.util.List;
+
 /**
  * Created by fpolizzi on 06.01.26
  */
 public interface UserDao {
 
-    User[] getUsers();
+    List<User> getUsers();
 }
