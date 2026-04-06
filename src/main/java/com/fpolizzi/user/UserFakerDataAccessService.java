80e779e8e9bb5f3e6a935ecb90bb21c526bab368
Franco Polizzi <me@fpolizzi.com>
2026-04-06 17:59:16 +0200

    feat: implement fake user generating

--- src/main/java/com/fpolizzi/user/UserFakerDataAccessService.java
+++ src/main/java/com/fpolizzi/user/UserFakerDataAccessService.java
@@ -1,6 +1,10 @@
 package com.fpolizzi.user;
 
+import net.datafaker.Faker;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * Created by fpolizzi on 06.04.26
@@ -9,6 +13,22 @@ public class UserFakerDataAccessService implements UserDao {
 
     @Override
     public List<User> getUsers() {
-        return List.of();
+
+        List<User> users = new ArrayList<>();
+
+        Faker faker = new Faker();
+
+        for (int i = 0; i < 20; i++) {
+
+            User user = new User(
+                    UUID.randomUUID(),
+                    faker.name().firstName(),
+                    faker.name().lastName()
+            );
+
+            users.add(user);
+        }
+
+        return users;
     }
 }
